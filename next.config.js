a4292227e02b13181e3a7a08bb81300ab5270f42
shiwoo.park <custardcream@kakao.com>
2023-02-26 20:00:39 +0900

    Chore: change api

--- next.config.js
+++ next.config.js
@@ -6,8 +6,7 @@ const nextConfig = {
     return [
       {
         source: "/api/firebasefunctions/:path*",
-        destination:
-          "https://asia-northeast3-devport-7dfd1.cloudfunctions.net/api/:path*",
+        destination: "https://devport-7dfd1.web.app/:path*",
       },
     ];
   },
